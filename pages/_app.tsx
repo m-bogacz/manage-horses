89522fee5b8c5e3c276f06e6f6953d57b7f79907
mateudev <mateuszbogacz.dev@gmail.com>
2023-04-05 22:40:24 +0200

    chore: initial project configuration

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Layout } from '@/components/layout/Layout';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ReactQueryDevtools />
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
