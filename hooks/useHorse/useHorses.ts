13a6500c920a355aa3f7ae7daa4abd79b7012703
mateudev <mateuszbogacz.dev@gmail.com>
2023-04-05 22:47:11 +0200

    feat: create useHorses hook

---
+++ hooks/useHorse/useHorses.ts
@@ -0,0 +1,14 @@
+import { HorseEntity } from '@/utils/types';
+import { useQuery } from '@tanstack/react-query';
+
+type FetchHorsesEntity = { horses: HorseEntity[] };
+
+export const fetchHorses = async (): Promise<FetchHorsesEntity> =>
+  await fetch('http://localhost:3001/api/horses').then((res) => res.json());
+
+export const useHorses = () => {
+  return useQuery({
+    queryKey: ['horses'],
+    queryFn: fetchHorses,
+  });
+};
