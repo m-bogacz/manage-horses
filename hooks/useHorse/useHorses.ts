7502b654e95d8b28044c17b7614a78b5065eabff
mateudev <mateuszbogacz.dev@gmail.com>
2023-04-06 18:55:02 +0200

    refactor(api): :zap: change api

--- hooks/useHorse/useHorses.ts
+++ hooks/useHorse/useHorses.ts
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 type FetchHorsesEntity = { horses: HorseEntity[] };
 
 export const fetchHorses = async (): Promise<FetchHorsesEntity> =>
-  await fetch('http://localhost:3001/api/horses').then((res) => res.json());
+  await fetch('http://localhost:3000/api/horses').then((res) => res.json());
 
 export const useHorses = () => {
   return useQuery({
