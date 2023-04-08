1cb45de07e914d716a466cdb45e3b9c75978d9af
mateudev <mateuszbogacz.dev@gmail.com>
2023-04-08 13:57:01 +0200

    refactor: :zap: change context type

--- apps/context/HorseContext.tsx
+++ apps/context/HorseContext.tsx
@@ -1,4 +1,5 @@
 import { createSafeContext } from '@/lib/createSafeContext';
+import { getDefaultphoto } from '@/utils/imageLoader/getDefualtPhoto';
 import { HorseEntity } from '@/utils/types';
 import { ReactNode } from 'react';
 
@@ -7,9 +8,20 @@ interface HorseProviderProps {
   value: HorseEntity;
 }
 
-const [useHorseContext, HorseContextProvider] = createSafeContext<HorseEntity>();
+type DefaultPhotoSrcType = { defaultPhotoSrc: string };
+
+interface ReturnValueType extends HorseEntity, DefaultPhotoSrcType {}
+
+const [useHorseContext, HorseContextProvider] = createSafeContext<ReturnValueType>();
 
 const HorseProvider = ({ children, value }: HorseProviderProps) => {
-  return <HorseContextProvider value={value}>{children}</HorseContextProvider>;
+  const defaultPhotoSrc = getDefaultphoto(value.images);
+
+  const returnValue = {
+    ...value,
+    defaultPhotoSrc,
+  };
+
+  return <HorseContextProvider value={returnValue}>{children}</HorseContextProvider>;
 };
 export { useHorseContext, HorseProvider };
