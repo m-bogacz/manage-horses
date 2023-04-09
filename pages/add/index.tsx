fccf8538e68bef91ee751186becf0b291e3c1c53
mateudev <mateuszbogacz.dev@gmail.com>
2023-04-09 09:19:11 +0200

    feat(pages): :sparkles: add a page with a form to add a horse

---
+++ pages/add/index.tsx
@@ -0,0 +1,10 @@
+import { Steps } from '@/module/steps/Steps';
+import React from 'react';
+
+export default function AddHorse() {
+  return (
+    <div>
+      <Steps />
+    </div>
+  );
+}
--- pages/horse/[name].tsx
+++ pages/horse/[name].tsx
@@ -3,7 +3,7 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import data from '@/json/data.json';
 import { HorseEntity } from '@/utils/types';
 import { TabMenu } from '@/module/tabMenu/TabMenu';
-import { Box, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { HorseProvider } from '@/apps/context/HorseContext';
 
 interface HorsePageProps {
@@ -13,7 +13,6 @@ interface HorsePageProps {
 export default function Horse({ horse }: HorsePageProps) {
   return (
     <HorseProvider value={horse}>
-      <Box display={{ base: 'block', lg: 'none' }}>AppBar</Box>
       <Flex h={'100vh'} w={'100%'} justifyContent="center">
         <TabMenu />
         <Flex display={{ base: 'none', md: 'flex' }}>
