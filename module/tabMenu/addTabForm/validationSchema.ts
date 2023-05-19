b7e9c762cf487d137dfee955a2a8cc353ff6275c
Rafał <rgrzesik@colorland.team>
2023-05-19 10:31:05 +0200

    fix(AddHorseForm): 🐛 Change minimal input length, error message language

--- module/tabMenu/addTabForm/validationSchema.ts
+++ module/tabMenu/addTabForm/validationSchema.ts
@@ -1,7 +1,7 @@
 import { z, date } from 'zod';
 
 export const addTabFormValidationSchema = z.object({
-  title: z.string().min(2, 'Tytuł musi mieć co najmniej 2 znaki'),
-  description: z.string().min(2, 'Opis musi mieć co najmniej 2 znaki'),
-  date: date({ required_error: 'Proszę wybrać datę' }),
+  title: z.string().min(5, 'Title must be 5 or more characters long'),
+  description: z.string().min(5, 'Description must be 5 or more characters long'),
+  date: date({ required_error: 'Please select date' }),
 });
